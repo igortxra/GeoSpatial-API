08474be212a02897007aafc75a25ca9bcc844e96
Igor Teixeira <igorxteixeira@gmail.com>
2026-02-16 11:55:33 -0300

    chore: speed up the ingestion script
    from ~91s and loading everything in memory to streaming by chunks in ~18s

--- ingestion.py
+++ ingestion.py
@@ -1,30 +1,45 @@
-from pandas import read_parquet
-from sqlalchemy import text
-from src.db import get_engine, init_db
+import io
+import time
 
+import polars as pl
+import psycopg2
 
-# TODO: Improve this script
+from src.db import init_db
+
 DATABASE_URL = "postgresql+psycopg2://postgres:postgres@localhost:5432/app"
-LINK_PARQUET = "./link_info.parquet.gz"
-SPEED_RECORDS_PARQUET = "./link_info.parquet.gz"
-
+LINK_PARQUET = "./utils/link_info.parquet.gz"
+SPEED_RECORDS_PARQUET = "./utils/duval_jan1_2024.parquet.gz"
 
 init_db(DATABASE_URL)
-engine = get_engine()
 
-# Link Info Upload
-raw_link_df = read_parquet("", columns=["link_id", "geo_json"])
+conn = psycopg2.connect(dbname="app", user="postgres", password="postgres", host="localhost", port="5432")
 
-raw_link_df.to_sql(
-    "links_raw",
-    engine,
-    if_exists="append",
-    index=False,
-    method="multi",
-)
 
-with engine.begin() as conn:
-    conn.execute(text("""
+def ingest_links():
+    cur = conn.cursor()
+    cur.execute(""" CREATE TEMP TABLE links_raw (
+        link_id BIGINT,
+        geo_json TEXT
+    )
+    """)
+
+    columns = ["link_id", "geo_json"]
+
+    lazy_df = pl.scan_parquet(LINK_PARQUET).select(columns)
+
+    cur = conn.cursor()
+    for batch in lazy_df.collect(engine="streaming").iter_slices(100_000):
+        buffer = io.StringIO()
+        batch.write_csv(buffer)
+        buffer.seek(0)
+
+        cur.copy_expert(
+            "COPY links_raw (link_id, geo_json) FROM STDIN WITH CSV HEADER",
+            buffer
+        )
+        conn.commit()
+
+    cur.execute("""
         INSERT INTO links (id, geom)
         SELECT
             link_id,
@@ -36,19 +51,45 @@ with engine.begin() as conn:
             )
         FROM links_raw
         ON CONFLICT (id) DO NOTHING
-    """))
-    conn.execute(text("DROP TABLE links_raw"))
+    """)
+
+    cur.execute("DROP TABLE links_raw")
+    cur.close()
 
 
-# Speed Records Ulpload
-raw_speed_records = read_parquet("./utils/duval_jan1_2024.parquet.gz", columns=["date_time", "average_speed", "link_id", "day_of_week", "period"])
+def ingest_speed_records():
+    cur = conn.cursor()
+    lazy_df = (
+        pl.scan_parquet(SPEED_RECORDS_PARQUET).select(["date_time", "average_speed", "link_id", "day_of_week", "period"])
+    )
 
-raw_speed_records = raw_speed_records.rename(columns={"date_time": "timestamp", "average_speed":"speed"})
+    for batch in lazy_df.collect(engine="streaming").iter_slices(n_rows=100_000):
+        buffer = io.StringIO()
+        batch.write_csv(buffer)
+        buffer.seek(0)
 
-raw_speed_records.to_sql(
-    "speed_records",
-    engine,
-    if_exists="append",
-    index_label="id",
-    method="multi",
-)
+        cur.copy_expert(
+            "COPY speed_records (timestamp, speed, link_id, day_of_week, period) FROM STDIN WITH CSV HEADER",
+            buffer
+        )
+        conn.commit()
+
+    cur.close()
+
+
+##################################################
+start_time = time.perf_counter()         # TIMER #
+##################################################
+
+ingest_links()
+ingest_speed_records()
+
+conn.close()
+
+
+####################################################
+end_time = time.perf_counter()                     #
+elapsed_time = end_time - start_time       # TIMER #
+print(f"Elapsed time: {elapsed_time:.4f} seconds") #
+####################################################
+
